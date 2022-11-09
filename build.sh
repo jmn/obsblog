b2aaece0ad5617e12386943456937c99183393d5
jmn <mail@jmnorlund.net>
2022-11-09 22:48:49 +0100

    submodule testing

--- build.sh
+++ build.sh
@@ -13,7 +13,7 @@ function copy_vault {
     # git clone https://github.com/MYORG/MYREPO ./vault
 
     # update submodule
-    git submodule init 
+    # git submodule init 
     git submodule update --recursive --remote
     # git submodule foreach git pull origin main
     ##### CHANGE STOP HERE
